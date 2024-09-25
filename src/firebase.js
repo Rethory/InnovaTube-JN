1423cb3bcecd81311f568eaed40b5c2969c885c9
Rethory <javier1najera@gmail.com>
2024-09-24 23:12:19 -0600

    inicio de firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-import 'firebase/auth'
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDBiLog0YP9KaJnPmglnhXJN2vfBcIv-oE",
   authDomain: "innovatube-jn.firebaseapp.com",
@@ -15,7 +11,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-// firebase.initializeApp(firebaseConfig)
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
-export default firebase.auth()
+export default auth;
